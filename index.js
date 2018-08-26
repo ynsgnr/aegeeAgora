c2866ec3af0d091e8ee55f7107d9e3a202a4e5a3
Yunus Güngör <yunusgnr@gmail.com>
2018-08-26 20:20:08 +0300

    Igoneren another warning message from other liblaries

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ import {name as appName} from './app.json';
 //Ignore isMounted() function error from react-native-navigation (It's coming from inside react native no problem)
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['Warning: componentWillReceiveProps is deprecated', 'Module RCTImageLoader']);
+YellowBox.ignoreWarnings(['Warning: componentWillUpdate is deprecated', 'Module RCTImageLoader']);
 
 AppRegistry.registerComponent(appName, () => App);

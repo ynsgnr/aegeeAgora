871613d97ba8a72950ac17f1e4f3e078b1323915
yunus <yunusgnr@gmail.com>
2018-09-16 16:56:18 -0700

    Fixed crash on location

--- app/scenes/Location.js
+++ app/scenes/Location.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, ActivityIndicator, Image, ScrollView, Linking, Dimensions} from 'react-native';
+import {StyleSheet, Text, View, ActivityIndicator, Image, ScrollView, Linking, Dimensions, Platform} from 'react-native';
 
 import {getLocationByKey} from '../actions/locations'
 import {getLocationEventsByDay} from '../actions/events'
